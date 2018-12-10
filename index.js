dbcc90dc46202b08be09a7829a31c1e07aa707f4
Naenae L <oneprincesslea@gmail.com>
2018-12-10 23:43:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+
+function getFirstSelector(selector){
+  return selector[i]; 
+} 
+
+function nestedTarget(){
+  document.querySelector (nested.target); 
+} 
+
+function increaseRankBy(n){
+  return parseInt(n.ranked-list); 
+} 
+
+function deepestChild(){
+  return querySelector(div.grand-node); 
+  
+}
